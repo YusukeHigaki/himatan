5f79ae6ce03994a8c1140986561f0f8cf3e7c960
Yusuke Higaki <yusuke.higaki@dzb.jp>
2014-05-20 15:16:25 +0900

    MESSAGE DONE

---
+++ src/Yusuke/HimatanBundle/Controller/api/messageApiController.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: higakiyuusuke
+ * Date: 2014/05/19
+ * Time: 16:24
+ */ 
---
+++ src/Yusuke/HimatanBundle/Service/MessageService.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: higakiyuusuke
+ * Date: 2014/05/20
+ * Time: 14:27
+ */ 
